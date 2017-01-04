f04ef484b7fb86a93c6634441e9c9bc194490534
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2017-01-04 12:58:43 -0800

    Refactor _getImageSize and additions to the README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-##A simple image picker with cropping. No additional libraries needed. 
+## A simple image picker with cropping.
+
+A vanilla javascript implementation of a UI component for a user to select an
+image from their file system for upload to a data store or rendering on a page.
+
+![Image of HTML drop box](/demo_images/imagepicker3.png)
+
+#### The Problem
+In the process of designing a web app that includes an image upload function, I
+needed to constrain the image dimensions to fit a predefined spec (in this case,
+images need to be 440px by 440px). The user should be able to select an image, see the resulting composition based on the constrained aspect ratio, and make some adjustment to derive an preferred composition before submitting. On submission, the module should crop the image to the proportions the user specified and also resize it the predetermined size. With a selection that doesn't meet the size minimum, a warning should be displayed instead.
+
+#### The Solution
+HTML's fileReader object allows a user to select a file from their file system using the __input__ tab with type="file". But a more pleasing interaction will allow a user to drag and drop a file from their desktop. The module initially displays a drop zone, with a size and aspect ratio based on config settings in the JS file. The HTML5 Drag and drop API is used to access the user's dropped file.     
+
+Canvas elements are implemented to both draw the chosen image on screen and to draw an overlay box on top of the image to demonstrate the final rendered image when the user taps the submit button. Image areas outside of the overlay are darkened to indicate the portion of the image that will be saved. The user can slide the overlay box either side to side or up and down to adjust the image cropping.
+
+![Image of Medieval Man with Sword](/demo_images/imagepicker2.jpg)
+
+The most sophisticated aspect of the module is a process that reads the raw binary data from a chosen to image to derive the image's pixel dimensions (currently limited to PNG and JPEG files). Whether the overlay moves up & down or side to side is determined by a function that evaluates the aspect ratio of the chose image to the aspect ratio of the final rendered image.
+
+### Configuration
+
+The values are in the "config" section define the final desired image size as
+well as the size of the preview window displayed on the page. Change these to your
+desired preferences.
+
+```javascript
+const RENDERED_IMAGE_WIDTH = 440;
+const RENDERED_IMAGE_HEIGHT = 440;
+const PREVIEW_IMAGE_WIDTH = 220;
+```
+
+The `submitRequest()` function should be rewritten to handle the final image data as needed. Currently it renders to the screen, presumably it should invoke an Ajax call to upload the image to a data store or some such thing.
+
+---
+
+The component is also available at [Codepen](http://codepen.io/billyham/pen/jVLaYP).
+
+No additional libraries are required.
--- src/script.js
+++ src/script.js
@@ -14,12 +14,18 @@
 
   /**
    * Use PREVIEW_IMAGE_WIDTH to set the onscreen pixel dimensions of the
-   * Imagepicker box to receive the drag and drop action. PREVIEW_IMAGE_HEIGHT
-   * will be equal to RENDERED_IMAGE_HEIGHT multplied by the ratio:
-   * PREVIEW_IMAGE_WIDTH / RENDERED_IMAGE_WIDTH
+   * Imagepicker box to receive the drag and drop action.
    * In pixels
    */
   const PREVIEW_IMAGE_WIDTH = 220;
+
+  /**
+   * The aspect ratio for preview image follows the aspect ratio of the rendered
+   * image. PREVIEW_IMAGE_HEIGHT will be equal to RENDERED_IMAGE_HEIGHT
+   * multplied by the ratio: PREVIEW_IMAGE_WIDTH / RENDERED_IMAGE_WIDTH.
+   * In pixels.
+   */
+  const PREVIEW_IMAGE_HEIGHT  = RENDERED_IMAGE_HEIGHT * PREVIEW_IMAGE_WIDTH / RENDERED_IMAGE_WIDTH;
 
   // =========================== Event Handlers ============================= //
   // Drag events
@@ -45,9 +51,8 @@
   const _getImageSize       = getImageSize;
 
   // ================================= Init ================================= //
-  const PREVIEW_IMAGE_HEIGHT  = RENDERED_IMAGE_HEIGHT * PREVIEW_IMAGE_WIDTH / RENDERED_IMAGE_WIDTH;
-  var _canvasWidth            = PREVIEW_IMAGE_WIDTH;
-  var _canvasHeight           = PREVIEW_IMAGE_HEIGHT;
+  var _canvasWidth       = PREVIEW_IMAGE_WIDTH;
+  var _canvasHeight      = PREVIEW_IMAGE_HEIGHT;
 
   var _isEditing         = false;
   var _overlayOriginX    = 0;
@@ -175,8 +180,6 @@
    * converted to arrayBuffer objects. On successfully loading the file,
    * continues to update UI by calling _applyImage().
    *
-   * @fires _applyImage
-   *
    * @param  {Array} tryFiles
    */
   function onChange(tryFiles){
@@ -227,10 +230,6 @@
    * Evaluates the aspect ration and pixel dimensions of a selected image file.
    * Launches methods to draw the image onto the canvas element and draw a crop
    * frame in the overlay canvas.
-   *
-   * @fires _setSize
-   * @fires _drawOverlay
-   * @fires _drawCroppedCanvas
    */
   function applyImage() {
     _setSize(PREVIEW_IMAGE_WIDTH, PREVIEW_IMAGE_HEIGHT);
@@ -319,6 +318,12 @@
     overlayCtx.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height);
   }
 
+  /**
+   * Present an error message when the picked image size fails to meet the
+   * desired minimum size, based on config settings.
+   *
+   * @param {bool} showAlert  Arg is true to show the image, false to hide it.
+   */
   function setSizeAlert(showAlert){
     let classes = showAlert ? '__warning' : '__warning __hide';
     let alertText = document.getElementById('px-warning');
@@ -326,7 +331,13 @@
     alertText.className = classes;
   }
 
-  // Crop overlay canvas
+  /**
+   * Draw a canvas overlay, showing the user a box over the picked image, with
+   * an aspect ratio based on the desired final rendered image from the config
+   * settings. Canvas overlay is updated with the movement of the user's mouse
+   * or touch.
+   * @param  {object} event Mouse or touch events inside the drop zone container.
+   */
   function drawOverlay(event){
     // The crop box size
     const cropSize = { width: PREVIEW_IMAGE_WIDTH, height: PREVIEW_IMAGE_HEIGHT };
@@ -373,6 +384,11 @@
     _overlayOriginY = (_currentY - cropHalf.height) * (RENDERED_IMAGE_HEIGHT / PREVIEW_IMAGE_HEIGHT);
   }
 
+
+  /**
+   * Render the final image, with cropping based on the user input from the
+   * overlay selection. Save final image data to var _croppedImageData.
+   */
   function drawCroppedCanvas(){
     const croppedCanvas = document.createElementNS('http://www.w3.org/1999/xhtml', 'canvas');
     croppedCanvas.width = RENDERED_IMAGE_WIDTH;
@@ -427,11 +443,76 @@
     };
   }
 
+  /**
+   * Evaluates the pixel size of an image and returns an object. Currently works
+   * for JPEG and PNG file types.
+   *
+   * @param  {arrayBuffer} arrayBuffer  Raw image data.
+   * @param  {string} imageType         File type, expects "image/png" or "image/jpeg"
+   *
+   * @return {object}  An object with properties for pixel dimensions of the image:
+   *                        width: {number}
+   *                        height: {number}
+   *                   Will return null if the evaluation fails.
+   */
   function getImageSize(arrayBuffer, imageType){
+
+    const MARKER_PREFIX           = 255;
+    const EXIF_APP1_MARKER        = 225;
+    const EXIF_APP1_CONFIRMATION  = 1165519206;
+    const EXIF_SUB_IFD            = 34665;
+    const START_OF_SCAN_MARKER    = 218;
+    const INTEL_BYTE              = 73;
+    const MOTOROLA_BYTE           = 77;
+
+    const EXIF_IMAGE_WIDTH        = 256;
+    const EXIF_IMAGE_HEIGHT       = 257;
+    // EXIF_ORIENTATION = 274
+    // EXIF_ORIENTATION = 112
+
+    const EXIF_SUB_IFD_WIDTH      = 40962;
+    const EXIF_SUB_IFD_HEIGHT     = 40963;
 
     let rawWidth = rawHeight = 0;
 
     const dataV = new DataView(arrayBuffer);
+
+    function startOfFrame(tag) {
+      if(
+        (tag >= 192 && tag <= 195) ||
+        (tag >= 197 && tag <= 199) ||
+        (tag >= 201 && tag <= 203) ||
+        (tag >= 205 && tag <= 207)
+      ) return true;
+    }
+
+    function subIFD(startByte, offsetToFirstIFD, i, isIntelAlign){
+      let IFDWidth = IFDHeight = 0;
+      let subOffset = dataV.getUint16(startByte + 9 + offsetToFirstIFD + 2 + (i * 12) + (isIntelAlign ? 8 : 10), isIntelAlign);
+
+      if (subOffset === 0) return false;
+      let fullSubOffset = startByte + 9 + subOffset;
+      let numOfSubs = dataV.getUint16(fullSubOffset, isIntelAlign);
+
+      // Bitmask to exit the loop if width and height are found;
+      let widthFlag = 1, heightFlag = 2, flags = 0;
+      for (let n = 0; n < numOfSubs; n++){
+
+        let entryItem = dataV.getUint16(fullSubOffset + 2 + (n * 12), isIntelAlign);
+
+        if (entryItem === EXIF_SUB_IFD_WIDTH){
+          IFDWidth = dataV.getUint32(fullSubOffset + 2 + (n * 12) + 8, isIntelAlign);
+          if (flags & heightFlag) return {width: IFDWidth, height: IFDHeight};
+          flags |= widthFlag;
+        }
+
+        if (entryItem === EXIF_SUB_IFD_HEIGHT){
+          IFDHeight = dataV.getUint32(fullSubOffset + 2 + (n * 12) + 8, isIntelAlign);
+          if (flags & widthFlag) return {width: IFDWidth, height: IFDHeight};
+          flags |= heightFlag;
+        }
+      }
+    }
 
     if (imageType === 'image/png'){
       rawWidth = dataV.getUint32(16);
@@ -439,92 +520,76 @@
       return { width: rawWidth, height: rawHeight };
 
     }else if (imageType === 'image/jpeg'){
+      // While(! start of scan marker){...  could be used in place of a for loop,
+      // but the for loop will guard against a corrupt file that lacks a start
+      // of scan marker.
       for(let x = 0; x < 40000; x++){
-        if (dataV.getUint8(x) === 255){
+        if (dataV.getUint8(x) === MARKER_PREFIX){
+          let byte = x + 1;
 
           // APP1 Marker (EXIF)
-          if (dataV.getUint8(x+1) === 225){
+          if (dataV.getUint8(byte) === EXIF_APP1_MARKER){
             // Check if EXIF header marker or APP1 Marker was false positive
-            if (dataV.getUint32(x+4) !== 1165519206) continue;
+            if (dataV.getUint32(byte + 3) !== EXIF_APP1_CONFIRMATION) continue;
 
             // 73 73: Intel byte align uses little endian
             // 77 77: Motorola byte align uses big endian
             let isIntelAlign = false;
-            if (dataV.getUint8(x+10) === 73 && dataV.getUint8(x+11) === 73) isIntelAlign = true;
+            if (dataV.getUint8(byte + 9) === INTEL_BYTE && dataV.getUint8(byte + 10) === INTEL_BYTE) isIntelAlign = true;
 
             // Offset to the first IFD
-            const offsetToFirstIFD = dataV.getUint32(x+14, isIntelAlign)
-            const entryCount = dataV.getUint16(x + 10 + offsetToFirstIFD, isIntelAlign);
+            const offsetToFirstIFD = dataV.getUint32(byte + 13, isIntelAlign)
+            const entryCount = dataV.getUint16(byte + 9 + offsetToFirstIFD, isIntelAlign);
             // Each entry is 12 bytes:
             //   2 bytes for tag number
             //   2 bytes for data type
             //   4 bytes for number of components
             //   4 bytes for data (or offset to data)
             for (let i = 0; i < entryCount; i++){
-              let tagNum = dataV.getUint16(x + 10 + offsetToFirstIFD + 2 + (i * 12), isIntelAlign)
-              // 274 is orientation
-              // 112 is orientation
+              let tagNum = dataV.getUint16(byte + 9 + offsetToFirstIFD + 2 + (i * 12), isIntelAlign);
 
               // Found Image Width (however for some files this is thumbnail width)
-              if (tagNum === 256){
-                let actualWidth = dataV.getUint16(x + 10 + offsetToFirstIFD + 2 + (i * 12) + 8, isIntelAlign);
+              if (tagNum === EXIF_IMAGE_WIDTH){
+                let actualWidth = dataV.getUint16(byte + 9 + offsetToFirstIFD + 2 + (i * 12) + 8, isIntelAlign);
                 if (!rawWidth) rawWidth = actualWidth;
               }
 
               // Found Image Height (however for some files this is thumbnail height)
-              if (tagNum === 257){
-                let actualHeight = dataV.getUint16(x + 10 + offsetToFirstIFD + 2 + (i * 12) + 8, isIntelAlign);
+              if (tagNum === EXIF_IMAGE_HEIGHT){
+                let actualHeight = dataV.getUint16(byte + 9 + offsetToFirstIFD + 2 + (i * 12) + 8, isIntelAlign);
                 if (!rawHeight) rawHeight = actualHeight;
               }
 
-              // Found EXIF SubIFD
-              if (tagNum === 34665){
-                let subOffset = dataV.getUint16(x + 10 + offsetToFirstIFD + 2 + (i * 12) + (isIntelAlign ? 8 : 10), isIntelAlign);
-                if (subOffset === 0) continue;
-                let fullSubOffset = x+10+subOffset;
-                let numOfSubs = dataV.getUint16(fullSubOffset, isIntelAlign);
-
-                // Bitmask to exit the loop if width and height are found;
-                let widthFlag = 1, heightFlag = 2, flags = 0;
-                for (let n = 0; n < numOfSubs; n++){
-
-                  let entryItem = dataV.getUint16(fullSubOffset + 2 + (n * 12), isIntelAlign);
-
-                  if (entryItem === 40962){
-                    rawWidth = dataV.getUint32(fullSubOffset + 2 + (n * 12) + 8, isIntelAlign);
-                    if (flags & heightFlag) return {width: rawWidth, height: rawHeight};
-                    flags |= widthFlag;
-                  }
-
-                  if (entryItem === 40963){
-                    rawHeight = dataV.getUint32(fullSubOffset + 2 + (n * 12) + 8, isIntelAlign);
-                    if (flags & widthFlag) return {width: rawWidth, height: rawHeight};
-                    flags |= heightFlag;
-                  }
-                }
+              // EXIF SubIFD marker
+              // EXIF SubIFD is authoritative. If height and width properties
+              // exist here, they take precedence and can be trusted to be
+              // definitive.
+              if (tagNum === EXIF_SUB_IFD){
+                const IFDResult = subIFD(byte, offsetToFirstIFD, i, isIntelAlign);
+                if (!IFDResult) continue;
+                if (!IFDResult.width || !IFDResult.height) return null;
+                return IFDResult;
               }
             }
           }
 
           // Start-Of-Frame marker
-          if (
-            ((dataV.getUint8(x+1) >= 192) && (dataV.getUint8(x+1) <= 195)) ||
-            ((dataV.getUint8(x+1) >= 197) && (dataV.getUint8(x+1) <= 199)) ||
-            ((dataV.getUint8(x+1) >= 201) && (dataV.getUint8(x+1) <= 203)) ||
-            ((dataV.getUint8(x+1) >= 205) && (dataV.getUint8(x+1) <= 207))
-          ){
-            if (!rawHeight) rawHeight = dataV.getUint16(x + 5);
-            if (!rawWidth) rawWidth = dataV.getUint16(x + 7);
+          if (startOfFrame(dataV.getUint8(byte))){
+            if (!rawHeight) rawHeight = dataV.getUint16(byte + 4);
+            if (!rawWidth) rawWidth = dataV.getUint16(byte + 6);
           }
 
-          //Start-Of-Scan marker (stop looking for markers)
-          if (dataV.getUint8(x+1) === 218) {
+          // Start-Of-Scan marker, stop looking for markers
+          if (dataV.getUint8(byte) === START_OF_SCAN_MARKER) {
             break;
           }
         }
       }
       // Return dimensions if valid
       if (rawWidth && rawHeight) return { width: rawWidth, height: rawHeight };
+
+      // Failed to find dimensions
+      return null;
 
     }else{
       // Unrecognized image type
